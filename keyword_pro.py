581898f8811aef215255c1d0d2ccd1b160d1ff2b
Jehyung Lee <trevor91@naver.com>
2017-12-28 17:42:23 +0900

    modified sql query & upgrade...many method

--- keyword_pro.py
+++ keyword_pro.py
@@ -5,39 +5,29 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 if __name__ == '__main__':
 	
-	query	= ''
-	start	= '2017.03.29'
-	end 	= '2017.07.31'
+	### 한 시점에 대해서 20개 키워드를 각각 start, end를 통해서 기사를 수집하고.
+	### 그 기사들을 기준으로. A라는 키워드 기사에 나머지 19개의 키워드 모두에대한 cosine similarity를 구함.
 
-	key1 = keyword(query, start, end)
-	key1.sqlConnect('','','')
-	key1.newsCrawling(newsList = True, newsCont = False)
+	# key1 = keyword(query, start, end)
+	key1 = keyword()
+	key1.sqlConnect('','root','')
+	period = key1.selectKeywordPeriod("미세먼지")
+	for s in period:
+		print(s)
+		key1.setKeyword(s["keyword"])
+		key1.setStart(s["start"])
+		key1.setEnd(s["end"])
+
+		key1.newsCrawling(newsList = True, newsCont = False)
+		# key1.setDBToNewsUrl()
+		# key1.newsCrawling(newsList = False, newsCont = True)
+	
+	# key1.newsCrawling(newsList = True, newsCont = False)
+
+	# key1.selectNewsList()
+	
+	# key1.setDBToNewsUrl()
+	# key1.newsCrawling(newsList = False, newsCont = True)
+
 	key1.sqlClose()
 	print('-'*50)
-	
-	# temp = key1.getNews()
-	# print(len(temp))
-	# for idx, s in enumerate(temp):
-	# 	print(s)
-	# 	if idx == 5:
-	# 		break
-	# print('-'*50)
-	
-
-	# testDic = key1.searchKeyword(testKeyword)
-	# # print(testDic)
-	# vect = DictVectorizer(sparse=False)
-	# tfidf_matrix = vect.fit_transform(testDic)
-	
-	# print('-'*50)
-	# print(tfidf_matrix)
-	# print('-'*50)
-	# print(tfidf_matrix[0:1])
-	# print('='*50)
-
-	# for i in range(len(tfidf_matrix)):
-	# 	print('*'*50)
-	# 	rst = cosine_similarity(tfidf_matrix[i:(i+1)], tfidf_matrix)
-	# 	print(rst)
-	# # next refer.
-	# # https://datascienceschool.net/view-notebook/3e7aadbf88ed4f0d87a76f9ddc925d69/
